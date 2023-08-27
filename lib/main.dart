d947f30e060e1f43bbe90f0dccbf8b7d87c23cdc
Diogo Vasques <49960268+DiVasques@users.noreply.github.com>
2023-08-27 19:17:00 -0300

    feat: implemented base splash screen (#1)
    * feat: implemented base splash screen
    
    * refactor: removed unused comments
    
    * fix: removed wrong asset on pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gami_acad/ui/routers/generic_router.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'GamiAcad',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          fontFamily: 'Montserrat',
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: const Color(0xFF1F5F02))),
+      onGenerateRoute: GenericRouter.generateRoute,
+      initialRoute: GenericRouter.splashRoute,
     );
   }
 }
